aef88ca4181a94cbe5e11e85aa060b01cfbef9fe
Aaron Bonetti <AaronMik Bonetti@gmail.com>
2019-05-29 20:19:26 -0400

    fixed some responsiveness issues

--- src/components/About Section/About.js
+++ src/components/About Section/About.js
@@ -12,7 +12,7 @@ import "./about-queries.scss"
         
         <div className="about__content__container">
           <p>
-            <span>Self-taught. self-driven. </span>
+            <span>Self-taught. Self-driven. </span>
          I'm a web designer/developer from Denver, Colorado. I strive to deliver responsive, engaging, and fast sites written in clean, sustainable code. When I'm not developing, you can find me honing my skills at the skatepark or pushing my limits at the parkour gym.  </p>
         </div>
         
